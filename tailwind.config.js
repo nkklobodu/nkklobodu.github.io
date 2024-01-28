e2aadd49a9effa9c0e01f16fc8ba613df96e5c45
Nathaniel Klobodu <107303661+nkklobodu@users.noreply.github.com>
2024-01-28 23:41:19 +0000

    Reorganized files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  // content: ["./dist/**/*.{html,js}"],
+  content: [
+    "./index.html",     // Your root HTML file
+    "./assets/js/*.js", // All JS files in assets/js directory
+    "./pages/**/*.html" // All HTML files in pages directory
+  ],
   theme: {
     extend: {},
   },
